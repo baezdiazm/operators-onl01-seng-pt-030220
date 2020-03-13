d97d783b5a21e22fe650ae7d30a06238a7d8613f
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-03-13 20:54:02 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,12 +12,13 @@ end
 
 
 def not_safe?(speed)
-  speed > 60? true : false
-  speed < 40? true : false
+
   if
     speed < 40 && speed > 60
     true
   else
     false
   end
+  speed > 60? true : false
+  speed < 40? true : false
 end
